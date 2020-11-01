d77711950fac01c1ae31c925260518c81f8f12a0
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-11-01 17:29:14 -0500

    add: logging + timing middleware

--- mod.ts
+++ mod.ts
@@ -3,6 +3,21 @@ import { Application } from "https://deno.land/x/oak@v6.3.1/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
+// logging middleware
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+});
+
+// time for requests middleware
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+});
+
 app.use(async (ctx) => {
   ctx.response.body = `
     {___     {__      {_         {__ __        {_       
